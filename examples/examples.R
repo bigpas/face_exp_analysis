85f26c693387a81a91be8d356dd9f0ab1dd0568e
bigpas <pasqui.dente@gmail.com>
2017-10-02 22:25:05 +0100

    typo

--- examples/examples.R
+++ examples/examples.R
@@ -6,39 +6,39 @@ source('R/sensitivity_score.R')
 target_labels <-
     c("Joy", "Anger", "Surprise", "Fear", "Disgust", "Sadness")
 
-targets_for_AFDES <- sapply(target_labels, function(x)
+targets_for_ADFES <- sapply(target_labels, function(x)
     paste0(x, " Evidence"))
 
 # Input files and Data preparation -----------------------------------------------------
-AFDES_raw_files <-
+ADFES_raw_files <-
     dir('raw-data/', pattern = '^Dump', full.names = TRUE)
-AFDES_raw_files
+ADFES_raw_files
 
 # problem with readr_tsv(); fread works great instead!
-AFDES_data <- lapply(AFDES_raw_files, function(x)
+ADFES_data <- lapply(ADFES_raw_files, function(x)
     data.table::fread(x, data.table = FALSE))
 
-names(AFDES_data) <- gsub("raw-data//|\\.txt$", "", AFDES_raw_files) %>%
+names(ADFES_data) <- gsub("raw-data//|\\.txt$", "", ADFES_raw_files) %>%
                         gsub('Dump\\d+_', '', .) %>%
                             gsub('\\s+|-', "_", .)
 
 
 # examples confidence scores -----------------------------------------------
 #single data frame
-confidence_score(df = AFDES_data[["North_Europ_F01sadnessface_Forward_7"]],
-                 target_labels_for_dataset = targets_for_AFDES)
+confidence_score(df = ADFES_data[["North_Europ_F01sadnessface_Forward_7"]],
+                 target_labels_for_dataset = targets_for_ADFES)
 
 #list of data frames
-lapply(AFDES_data, function(dataset)
+lapply(ADFES_data, function(dataset)
     confidence_score(df = dataset,
-                     target_labels_for_dataset = targets_for_AFDES)) %>%
+                     target_labels_for_dataset = targets_for_ADFES)) %>%
     plyr::ldply(.id = "file_Id")
 
 # examples sensitivity scores ---------------------------------------------
-sensitivity_scores(df = AFDES_data[["North_Europ_F01sadnessface_Forward_7"]],
-                   target_labels_for_dataset = targets_for_AFDES)
+sensitivity_scores(df = ADFES_data[["North_Europ_F01sadnessface_Forward_7"]],
+                   target_labels_for_dataset = targets_for_ADFES)
 
-lapply(AFDES_data, function(dataset)
+lapply(ADFES_data, function(dataset)
     sensitivity_scores(df = dataset,
-                       target_labels_for_dataset = targets_for_AFDES)) %>%
+                       target_labels_for_dataset = targets_for_ADFES)) %>%
     plyr::ldply(.id = "file_Id")
